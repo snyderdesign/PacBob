c2461bb182dcc29595944d54d764c85884ab8598
Snyder <bobsnyder@Roberts-MacBook-Air.local>
2016-03-19 10:45:39 -0700

    CLEANED-TABS
    It looks prettier.

--- pacCode.js
+++ pacCode.js
@@ -1,49 +1,49 @@
- var world = randomWorld([
-     [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,4],
-     [13,2,0,0,0,0,0,0,0,0,0,0,0,0,0,2,12],
-     [13,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,12],
-     [13,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,12],
-     [13,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,12],
-     [13,0,1,1,1,1,1,1,2,1,1,1,1,1,1,0,12],
-     [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
-     [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
-     [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
-     [13,0,0,0,0,2,0,0,0,0,0,2,0,0,0,0,12],
-     [4,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,4]
-   ]
- );
+var world = randomWorld([
+  [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,4],
+  [13,2,0,0,0,0,0,0,0,0,0,0,0,0,0,2,12],
+  [13,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,12],
+  [13,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,12],
+  [13,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,12],
+  [13,0,1,1,1,1,1,1,2,1,1,1,1,1,1,0,12],
+  [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
+  [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
+  [13,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,12],
+  [13,0,0,0,0,2,0,0,0,0,0,2,0,0,0,0,12],
+  [4,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,4]
+]
+);
 //this is the function that runs in real-time and keeps updating each element
 function runTime() {
-    displayWorld();
-    displayPacman();
-    displayPacbro();
-    //I know there must be a way to contain all the active objects
-    //but I'm not there yet.  I'll finish later.
-    pacObjectMove(pacman);
-    pacObjectMove(pacbro);
-    ghostRunner(ghostRed);
-    ghostRunner(ghostGreen);
-    displayGhost();
-    updateScore();
-    if(cherry.count<60){
-      cherry.count++;
-    } else if(cherry.init===3){
-    } else {
-      npcObjectMove(cherry);
-      displayCherry();
-      cherryCrash(go);
-    }
-    if(ghostCrashRed(go)===1){
-      document.getElementById('worlds').innerHTML = "<div class='gameOverOne'>Game Over</br> Red Ghost Strikes Again!</div>";
-      return;
-    }
-    if(ghostCrashGreen(go)===1){
-      document.getElementById('worlds').innerHTML = "<div class='gameOverOne'>Game Over</br> Green Ghost Strikes Again!</div>";
-      return;
-    }
-    //setTimeout I found when looking for a time display.
-    //It does a function after waiting a specified amount of time.
-    setTimeout(runTime, 50);
+  displayWorld();
+  displayPacman();
+  displayPacbro();
+  //I know there must be a way to contain all the active objects
+  //but I'm not there yet.  I'll finish later.
+  pacObjectMove(pacman);
+  pacObjectMove(pacbro);
+  ghostRunner(ghostRed);
+  ghostRunner(ghostGreen);
+  displayGhost();
+  updateScore();
+  if(cherry.count<60){
+    cherry.count++;
+  } else if(cherry.init===3){
+  } else {
+    npcObjectMove(cherry);
+    displayCherry();
+    cherryCrash(go);
+  }
+  if(ghostCrashRed(go)===1){
+    document.getElementById('worlds').innerHTML = "<div class='gameOverOne'>Game Over</br> Red Ghost Strikes Again!</div>";
+    return;
+  }
+  if(ghostCrashGreen(go)===1){
+    document.getElementById('worlds').innerHTML = "<div class='gameOverOne'>Game Over</br> Green Ghost Strikes Again!</div>";
+    return;
+  }
+  //setTimeout I found when looking for a time display.
+  //It does a function after waiting a specified amount of time.
+  setTimeout(runTime, 50);
 }
 //end of runtime function
 var score = 0;
@@ -292,68 +292,68 @@ function moveObject(movableObject, yChange, xChange){
 }
 //want to change the incements to a variable stored in the pac objects and the checks a standard function.
 document.onkeydown = function(e){ //my way of checking for key inputs
-if(e.keyCode == 37){
-  //moveObject(javascript object to be moved, change in y, change in x)
-  pacman.dir = 1;
-//  moveObject(pacman, 0, -0.5);
-//  activSquare(pacman);
-}
-else if(e.keyCode == 39){
-  pacman.dir = 2;
-//  moveObject(pacman, 0, 0.5);
-//  activSquare(pacman);
-}
+  if(e.keyCode == 37){
+    //moveObject(javascript object to be moved, change in y, change in x)
+    pacman.dir = 1;
+    //  moveObject(pacman, 0, -0.5);
+    //  activSquare(pacman);
+  }
+  else if(e.keyCode == 39){
+    pacman.dir = 2;
+    //  moveObject(pacman, 0, 0.5);
+    //  activSquare(pacman);
+  }
 
-else if(e.keyCode == 38){
-  pacman.dir = 3;
-//  moveObject(pacman, -0.5, 0);
-//  activSquare(pacman);
-}
-else if(e.keyCode == 40){
-  pacman.dir = 4;
-//  moveObject(pacman, 0.5, 0);
-//  activSquare(pacman);
-}
-if(e.keyCode == 65){
-  pacbro.dir = 1;
-//  moveObject(pacbro, 0, -0.5);
-//  activSquare(pacbro);
-}
-else if(e.keyCode == 68){
-  pacbro.dir = 2;
-//  moveObject(pacbro, 0, 0.5);
-//  activSquare(pacbro);
-}
-else if(e.keyCode == 87){
-  pacbro.dir = 3;
-//  moveObject(pacbro, -0.5, 0);
-//  activSquare(pacbro);
-}
-else if(e.keyCode == 83){
-  pacbro.dir = 4;
-//  moveObject(pacbro, 0.5, 0);
-//  activSquare(pacbro);
-}
+  else if(e.keyCode == 38){
+    pacman.dir = 3;
+    //  moveObject(pacman, -0.5, 0);
+    //  activSquare(pacman);
+  }
+  else if(e.keyCode == 40){
+    pacman.dir = 4;
+    //  moveObject(pacman, 0.5, 0);
+    //  activSquare(pacman);
+  }
+  if(e.keyCode == 65){
+    pacbro.dir = 1;
+    //  moveObject(pacbro, 0, -0.5);
+    //  activSquare(pacbro);
+  }
+  else if(e.keyCode == 68){
+    pacbro.dir = 2;
+    //  moveObject(pacbro, 0, 0.5);
+    //  activSquare(pacbro);
+  }
+  else if(e.keyCode == 87){
+    pacbro.dir = 3;
+    //  moveObject(pacbro, -0.5, 0);
+    //  activSquare(pacbro);
+  }
+  else if(e.keyCode == 83){
+    pacbro.dir = 4;
+    //  moveObject(pacbro, 0.5, 0);
+    //  activSquare(pacbro);
+  }
 }
 
 function ghostRunner(currentGhost){
-//console.log("runred");
-if(Math.ceil(Math.random()*10)<=6){
+  //console.log("runred");
+  if(Math.ceil(Math.random()*10)<=6){
 
-} else if (Math.ceil(Math.random()*10)===7){
-  currentGhost.dir = 1;
-}
-else if (Math.ceil(Math.random()*10)===8){
-  currentGhost.dir = 2;
-}
-else if (Math.ceil(Math.random()*10)===9){
-  currentGhost.dir = 3;
-}
-else if (Math.ceil(Math.random()*10)===10){
-  currentGhost.dir = 4;
-}
+  } else if (Math.ceil(Math.random()*10)===7){
+    currentGhost.dir = 1;
+  }
+  else if (Math.ceil(Math.random()*10)===8){
+    currentGhost.dir = 2;
+  }
+  else if (Math.ceil(Math.random()*10)===9){
+    currentGhost.dir = 3;
+  }
+  else if (Math.ceil(Math.random()*10)===10){
+    currentGhost.dir = 4;
+  }
 
-npcObjectMove(currentGhost);
+  npcObjectMove(currentGhost);
 }
 
 function pacObjectMove(currentPac){
@@ -372,83 +372,83 @@ function pacObjectMove(currentPac){
   }
 }
 function npcObjectMove(currentObject){
-var ghostTempX = currentObject.x;
-var ghostTempY = currentObject.y;
-if(currentObject.dir === 1){
-  moveObject(currentObject, 0, -0.1);
-  if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
-    currentObject.dir = 2;
+  var ghostTempX = currentObject.x;
+  var ghostTempY = currentObject.y;
+  if(currentObject.dir === 1){
+    moveObject(currentObject, 0, -0.1);
+    if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
+      currentObject.dir = 2;
+    }
   }
-}
-else if(currentObject.dir === 2){
-  moveObject(currentObject, 0, 0.1);
-  if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
-    currentObject.dir = 3;
+  else if(currentObject.dir === 2){
+    moveObject(currentObject, 0, 0.1);
+    if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
+      currentObject.dir = 3;
+    }
   }
-}
-else if(currentObject.dir === 3){
-  moveObject(currentObject, -0.1, 0);
-  if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
-    currentObject.dir = 4;
+  else if(currentObject.dir === 3){
+    moveObject(currentObject, -0.1, 0);
+    if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
+      currentObject.dir = 4;
+    }
   }
-}
-else if(currentObject.dir === 4){
-  moveObject(currentObject, 0.1, 0);
-  if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
-    currentObject.dir = 1;
+  else if(currentObject.dir === 4){
+    moveObject(currentObject, 0.1, 0);
+    if(ghostTempX === currentObject.x && ghostTempY === currentObject.y){
+      currentObject.dir = 1;
+    }
   }
-}
 }
 //this function compares objects to map objects... it does not compare object to object
 function activSquare(squareObject){
   var objectVOne = world[Math.floor(squareObject.y)][Math.floor(squareObject.x)];
   var objectVTwo = world[Math.ceil(squareObject.y)][Math.ceil(squareObject.x)];
-    if(objectVOne === 3){
-      world[Math.floor(squareObject.y)][Math.floor(squareObject.x)] = 2;
-      squareObject.charge += 5;
-    } else if(objectVOne === 1){
-      world[Math.floor(squareObject.y)][Math.floor(squareObject.x)] = 2;
-      squareObject.charge += 1;
-    } else if(objectVTwo === 3){
-      world[Math.ceil(squareObject.y)][Math.ceil(squareObject.x)] = 2;
-      squareObject.charge += 5;
-    } else if(objectVTwo === 1){
-      world[Math.ceil(squareObject.y)][Math.ceil(squareObject.x)] = 2;
-      squareObject.charge += 1;
-    }
+  if(objectVOne === 3){
+    world[Math.floor(squareObject.y)][Math.floor(squareObject.x)] = 2;
+    squareObject.charge += 5;
+  } else if(objectVOne === 1){
+    world[Math.floor(squareObject.y)][Math.floor(squareObject.x)] = 2;
+    squareObject.charge += 1;
+  } else if(objectVTwo === 3){
+    world[Math.ceil(squareObject.y)][Math.ceil(squareObject.x)] = 2;
+    squareObject.charge += 5;
+  } else if(objectVTwo === 1){
+    world[Math.ceil(squareObject.y)][Math.ceil(squareObject.x)] = 2;
+    squareObject.charge += 1;
   }
-function ghostCrashRed(validCrash){
-if((Math.sqrt((pacman.y-ghostRed.y)*(pacman.y-ghostRed.y)))<=.7 && (Math.sqrt((pacman.x-ghostRed.x)*(pacman.x-ghostRed.x)))<=.7){
-  validCrash = 1;
-  return validCrash;
-} else if((Math.sqrt((pacbro.y-ghostRed.y)*(pacbro.y-ghostRed.y)))<=.7 && (Math.sqrt((pacbro.x-ghostRed.x)*(pacbro.x-ghostRed.x)))<=.7){
-  validCrash = 1;
-  return validCrash;
 }
+function ghostCrashRed(validCrash){
+  if((Math.sqrt((pacman.y-ghostRed.y)*(pacman.y-ghostRed.y)))<=.7 && (Math.sqrt((pacman.x-ghostRed.x)*(pacman.x-ghostRed.x)))<=.7){
+    validCrash = 1;
+    return validCrash;
+  } else if((Math.sqrt((pacbro.y-ghostRed.y)*(pacbro.y-ghostRed.y)))<=.7 && (Math.sqrt((pacbro.x-ghostRed.x)*(pacbro.x-ghostRed.x)))<=.7){
+    validCrash = 1;
+    return validCrash;
+  }
 }
 function ghostCrashGreen(validCrash){
-if((Math.sqrt((pacman.y-ghostGreen.y)*(pacman.y-ghostGreen.y)))<=.7 && (Math.sqrt((pacman.x-ghostGreen.x)*(pacman.x-ghostGreen.x)))<=.7){
-  validCrash = 1;
-  return validCrash;
-} else if((Math.sqrt((pacbro.y-ghostGreen.y)*(pacbro.y-ghostGreen.y)))<=.7 && (Math.sqrt((pacbro.x-ghostGreen.x)*(pacbro.x-ghostGreen.x)))<=.7){
-  validCrash = 1;
-  return validCrash;
-}
+  if((Math.sqrt((pacman.y-ghostGreen.y)*(pacman.y-ghostGreen.y)))<=.7 && (Math.sqrt((pacman.x-ghostGreen.x)*(pacman.x-ghostGreen.x)))<=.7){
+    validCrash = 1;
+    return validCrash;
+  } else if((Math.sqrt((pacbro.y-ghostGreen.y)*(pacbro.y-ghostGreen.y)))<=.7 && (Math.sqrt((pacbro.x-ghostGreen.x)*(pacbro.x-ghostGreen.x)))<=.7){
+    validCrash = 1;
+    return validCrash;
+  }
 }
 function cherryCrash(){
-var parenting = document.getElementById("bodys");
-var childing = document.getElementById("cherrys");
-if((Math.sqrt((pacman.y-cherry.y)*(pacman.y-cherry.y)))<=1 && (Math.sqrt((pacman.x-cherry.x)*(pacman.x-cherry.x)))<=1){
-  validCrash = 1;
-  pacman.charge+=20;
-  cherry.init=3;
-  parenting.removeChild(childing);
-  return;
-} else if((Math.sqrt((pacbro.y-cherry.y)*(pacbro.y-cherry.y)))<=1 && (Math.sqrt((pacbro.x-cherry.x)*(pacbro.x-cherry.x)))<=1){
-  validCrash = 1;
-  pacbro.charge+=20;
-  cherry.init= 3;
-  parenting.removeChild(childing);
-  return;
-}
+  var parenting = document.getElementById("bodys");
+  var childing = document.getElementById("cherrys");
+  if((Math.sqrt((pacman.y-cherry.y)*(pacman.y-cherry.y)))<=1 && (Math.sqrt((pacman.x-cherry.x)*(pacman.x-cherry.x)))<=1){
+    validCrash = 1;
+    pacman.charge+=20;
+    cherry.init=3;
+    parenting.removeChild(childing);
+    return;
+  } else if((Math.sqrt((pacbro.y-cherry.y)*(pacbro.y-cherry.y)))<=1 && (Math.sqrt((pacbro.x-cherry.x)*(pacbro.x-cherry.x)))<=1){
+    validCrash = 1;
+    pacbro.charge+=20;
+    cherry.init= 3;
+    parenting.removeChild(childing);
+    return;
+  }
 }
